0d286499e8bff876c5b6946288f42967f5462bdf
MakovWait <maxkv258@gmail.com>
2025-05-09 17:57:59 +0200

    Range -> RangeF to avoid System ns collisions

---
+++ src/Lofi2D.Tests/src/Math/RangeTests.cs
@@ -0,0 +1,24 @@
+﻿using Lofi2D.Math;
+
+namespace Lofi2D.Tests.Math;
+
+public class RangeFTests
+{
+    [Test]
+    public void OrderedTest()
+    {
+        var range = new RangeF(2, 1);
+        Assert.Multiple(() =>
+        {
+            Assert.That(range.Start, Is.EqualTo(2f));
+            Assert.That(range.End, Is.EqualTo(1f));
+        });
+
+        var ordered = range.Ordered;
+        Assert.Multiple(() =>
+        {
+            Assert.That(ordered.Start, Is.EqualTo(1f));
+            Assert.That(ordered.End, Is.EqualTo(2f));
+        });
+    }
+}
---
+++ src/Lofi2D/Math/RangeF.cs
@@ -0,0 +1,32 @@
+﻿namespace Lofi2D.Math;
+
+/// <summary>
+/// Range of float values.
+/// </summary>
+public readonly struct RangeF(float start, float end)
+{
+    public float Start { get; init; } = start;
+    public float End { get; init; } = end;
+
+    public bool Contains(float v) => v >= Start && v <= End;
+
+    public float Lerp(float progress)
+    {
+        return Mathf.Lerp(Start, End, progress);
+    }
+
+    public RangeF Clamp(float min, float max)
+    {
+        return new RangeF(Mathf.Clamp(Start, min, max), Mathf.Clamp(End, min, max));
+    }
+    
+    public RangeF Ordered => new(
+        Mathf.Min(Start, End),
+        Mathf.Max(Start, End)
+    );
+    
+    public override string ToString()
+    {
+        return $"[{Start}, {End}]";
+    }
+}
