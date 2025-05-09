3606d8bfff7b05abf423cc7863a789bb9525b72d
MakovWait <maxkv258@gmail.com>
2025-05-09 09:54:56 +0200

    range

---
+++ src/Lofi2D/Math/Range.cs
@@ -0,0 +1,23 @@
+﻿namespace Lofi2D.Math;
+
+/// <summary>
+/// Range of float values.
+/// </summary>
+public readonly struct Range(float start, float end)
+{
+    public readonly float Start = start;
+
+    public readonly float End = end;
+
+    public bool Contains(float v) => v >= Start && v <= End;
+
+    public float Lerp(float progress)
+    {
+        return Mathf.Lerp(Start, End, progress);
+    }
+
+    public Range Clamp(float min, float max)
+    {
+        return new Range(Mathf.Clamp(Start, min, max), Mathf.Clamp(End, min, max));
+    }
+}
